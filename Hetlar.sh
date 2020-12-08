d2821519ab39edcd460789c20928d377a7978a59
cccvev <75307540+cccvev@users.noreply.github.com>
2020-12-08 18:05:20 +0200

    Add files via upload

---
+++ Hetlar.sh
@@ -0,0 +1,82 @@
+
+clear
+-command_not_found_handle() {
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+hi-HETLAR(){
+
+#R='\e[107m'
+#r='\e[1;31m'
+#g='\e[1;32m'
+#b='\e[1;34m'
+#p='\e[1;35m'
+#c='\e[1;36m'
+#w='\e[1;37m'
+#y='\e[1;33m'
+#o='\033[0m'
+#B='\033[90;1m'
+#########################
+#B=" \033[1;30m " # Black
+#B=" \033[1;31m " # Red
+#B=" \033[1;32m " # Green
+#B=" \033[1;33m " # Yellow
+#B=" \033[1;34m " # Blue
+#B=" \033[1;35m " # Purple
+#B=" \033[1;36m " # Cyan
+#B=" \033[1;37m " # White
+
+printf "\033[1;31m ▄▄    ▄▄\033[1;32m  ▄▄▄▄▄▄▄▄\033[1;33m  ▄▄▄▄▄▄▄▄\033[1;34m  ▄▄      \033[1;35m     ▄▄   \033[1;36m  ▄▄▄▄▄▄   \n"
+printf "\033[1;31m ██    ██\033[1;32m  ██▀▀▀▀▀▀\033[1;33m  ▀▀▀██▀▀▀\033[1;34m  ██      \033[1;35m    ████  \033[1;36m  ██▀▀▀▀██ \n"
+printf "\033[1;31m ██    ██\033[1;32m  ██      \033[1;33m     ██   \033[1;34m  ██      \033[1;35m    ████  \033[1;36m  ██    ██ \n"
+printf "\033[1;31m ████████\033[1;32m  ███████ \033[1;33m     ██   \033[1;34m  ██      \033[1;35m   ██  ██ \033[1;36m  ███████  \n"
+printf "\033[1;31m ██    ██\033[1;32m  ██      \033[1;33m     ██   \033[1;34m  ██      \033[1;35m   ██████ \033[1;36m  ██  ▀██▄ \n"
+printf "\033[1;31m ██    ██\033[1;32m  ██▄▄▄▄▄▄\033[1;33m     ██   \033[1;34m  ██▄▄▄▄▄▄\033[1;35m  ▄██  ██▄\033[1;36m  ██    ██ \n"
+printf "\033[1;31m ▀▀    ▀▀\033[1;32m  ▀▀▀▀▀▀▀▀\033[1;33m     ▀▀   \033[1;34m  ▀▀▀▀▀▀▀▀\033[1;35m  ▀▀    ▀▀\033[1;36m  ▀▀    ▀▀▀ \n"
+
+
+}
+
+
+
+
+cmatrix -C red -s
+
+
+
+R='\e[107m'
+r='\e[1;31m'
+g='\e[1;32m'
+b='\e[1;34m'
+p='\e[1;35m'
+c='\e[1;36m'
+w='\e[1;37m'
+y='\e[1;33m'
+o='\033[0m'
+B='\033[90;1m'
+
+echo -e "$g               Welcome Back HETLAR  $R$w"
+#toilet -f mono12 -F border hetlar | lolcat
+
+echo -e "$o"
+hi-HETLAR
+
+PS1='\[\033[1;31m\]┌────\033[1;30mHaCk And Sec\033[1;31m────────────────────\033[1;37m[\T]\e[1;31m─┐ 
+└>HETLAR:~# \033[1;37m'
+#PS1='\$ 
+#'
+
+
+
+alias x='exit'
+alias c='clear'
+alias py='python'
+alias n='nano'
+alias b='bash'
+alias py2='python2'
+alias ch='chmod +x'
+alias g='git clone'
+alias r='rm -rif'
+alias sd='cd /sdcard'
+alias l='ls'
+alias la='ls -a'
+alias p='php'
